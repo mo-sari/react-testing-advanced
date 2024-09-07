0266886790d7400a453860d7636c41b5589c372a
mo-sari <mohsen.ansari.bim@gmail.com>
2024-09-07 15:09:04 +0330

    more work on act

--- src/components/repositories/RepositiresListItem.test.js
+++ src/components/repositories/RepositiresListItem.test.js
@@ -1,13 +1,7 @@
 import { screen, render, within } from "@testing-library/react";
 import RepositiriesListItem from "./RepositoriesListItem";
 import { MemoryRouter } from "react-router";
-const waiting = () => {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, 100);
-  });
-};
+
 const renderComponent = () => {
   const repository = {
     full_name: "react.js",
@@ -27,12 +21,13 @@ const renderComponent = () => {
 test("check if repos link is displayed", async () => {
   renderComponent();
 
-  screen.debug();
-  await waiting();
-  screen.debug();
+  const img = await screen.findByRole("img", { name: /javascript/i });
+
+  expect(img).toBeInTheDocument();
 });
 
-// in repo....ListItem we are chaning a state so we would get
-// act warning, we could use act function in a non jest tesing
-// environment but now we can use stuff that use the act concept
-// under the hood, like screen.findBy...
+// for the image we needed an accessible name which we did assign
+// using aria-label
+// and now this way we are testing the repo...ListItem component
+// and in that component there is a part(FileIcon) that has a state
+// that changes dynamicaly based on our input
