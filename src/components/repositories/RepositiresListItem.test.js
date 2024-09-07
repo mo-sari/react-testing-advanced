69b7b48c24676528487a7d78b91c2fdd8520f835
mo-sari <mohsen.ansari.bim@gmail.com>
2024-09-07 18:09:20 +0330

    looking for link which failed

--- src/components/repositories/RepositiresListItem.test.js
+++ src/components/repositories/RepositiresListItem.test.js
@@ -1,14 +1,13 @@
 import { screen, render } from "@testing-library/react";
 import RepositiriesListItem from "./RepositoriesListItem";
-import { MemoryRouter } from "react-router";
+import { MemoryRouter, resolvePath } from "react-router";
 
 const renderComponent = () => {
   const repository = {
-    full_name: "facebook/",
-    language: "javascript",
-    description:
-      "a powerfull library used for building interactive front end projects",
-    owner: "Meta",
+    full_name: "facebook/react",
+    language: "Javascript",
+    description: "A js library",
+    owner: "facebook",
     name: "react",
     html_url: "html",
   };
@@ -20,10 +19,19 @@ const renderComponent = () => {
   );
   return { repository };
 };
+
 test("check if repos link is displayed", async () => {
   const repository = renderComponent();
   await screen.findByRole("img", { name: /javascript/i });
   const link = screen.getByRole("link", { name: "github repo" });
   expect(link).toHaveAttribute("href", repository.html_url);
   // expect(link).toBeInTheDocument();
+});
+
+test("shows a fileicon with the appropriate icon", async () => {
+  renderComponent();
+
+  const icon = await screen.findByRole("img", { name: /javascript/i });
+
+  expect(icon).toHaveClass("js-icon");
 });
