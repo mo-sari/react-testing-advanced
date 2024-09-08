55bcc618fb07b021568f4def5177998a9c6eb0e5
mo-sari <mohsen.ansari.bim@gmail.com>
2024-09-08 20:05:55 +0330

    we now have two links specifily for each language

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -6,14 +6,13 @@ import HomeRoute from "./HomeRoute";
 
 const handlers = [
   rest.get("/api/repositories", (req, res, ctx) => {
-    const query = req.url.searchParams.get("q");
-    console.log(query);
+    const language = req.url.searchParams.get("q").split("language:")[1];
 
     return res(
       ctx.json({
         items: [
-          { id: 1, full_name: "full name!!" },
-          { id: 2, full_name: "other name!!" },
+          { id: 1, full_name: `${language}_one` },
+          { id: 2, full_name: `${language}_two` },
         ],
       })
     );
@@ -38,9 +37,6 @@ test("renders two links for each language", async () => {
       <HomeRoute />
     </MemoryRouter>
   );
-
-  // we are going to loop over languages, for each of them make sure
-  // we see two links and Assert that the links have the appropriate full_name
 
   await pause();
   screen.debug();
