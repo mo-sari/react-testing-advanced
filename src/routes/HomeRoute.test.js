f0429029c6ada910ce59aabdd73c97674986fb54
mo-sari <mohsen.ansari.bim@gmail.com>
2024-09-08 19:55:01 +0330

    first inspection of the data recieved from our fake server

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -4,21 +4,8 @@ import { rest } from "msw";
 import { MemoryRouter } from "react-router";
 import HomeRoute from "./HomeRoute";
 
-// STEP 2 of MSW Setup:
-// we first take a look at our request in chrome to see to what
-// URL we are sending our request.
-// then in HomeRoute we would look to the child components the see
-// when data was fetched in HomeRoute, what properties do we pass down from it
-
-// STEP 3
-// creating a MSW handler to intercept the request and return some
-// fake data for our component to use
 const handlers = [
   rest.get("/api/repositories", (req, res, ctx) => {
-    // we do not specify queryStrings in the url,
-    // queryStrings are in req argument
-
-    // below we are taking the query string from our url
     const query = req.url.searchParams.get("q");
     console.log(query);
 
@@ -36,15 +23,12 @@ const handlers = [
 const server = setupServer(...handlers);
 
 beforeAll(() => {
-  // runs before all tests in this file
   server.listen();
 });
 afterEach(() => {
-  // runs after each test in this file
   server.resetHandlers();
 });
 afterAll(() => {
-  // runs after all tests in this file
   server.close();
 });
 
@@ -55,10 +39,17 @@ test("renders two links for each language", async () => {
     </MemoryRouter>
   );
 
-  // with this below, we won't see any links for each item and that is because
-  // we are faking the request with MSW but we're not waiting for the answer at all
-  screen.debug();
-
   // we are going to loop over languages, for each of them make sure
   // we see two links and Assert that the links have the appropriate full_name
+
+  await pause();
+  screen.debug();
 });
+
+const pause = () => {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, 1000);
+  });
+};
